1d2f136ef654b89078e2daf4c68a68ab25be95b0
fwcd <fwcdmail@gmail.com>
2019-03-15 03:05:27 +0100

    Factor out WSL-path expansion into separate function

--- Sources/SKSupport/FileSystem.swift
+++ Sources/SKSupport/FileSystem.swift
@@ -22,6 +22,27 @@ public var homeDirectoryForCurrentUser: AbsolutePath {
   return AbsolutePath(NSHomeDirectory())
 }
 
+/// Expands a leading Windows drive letter in a file path ('/d:/...') to the WSL equivalent ('/mnt/d/...')
+/// This, however, only applies to the special case where a native Windows
+/// LSP client/editor is used while this language server runs via WSL.
+public func expandWSL(path: String) -> String {
+  #if os(Linux)
+    // Support the special case where a user runs an
+    // editor on Windows with the language server through
+    // Windows Subsystem for Linux (WSL).
+    // In this case, the editor will attempt to pass paths such as
+    // '/d:/...' to the language server which, however, expects
+    // them to be formatted as '/mnt/d/...'.
+    if let _ = windowsPathRegex.firstMatch(in: path, range: NSMakeRange(0, path.length)) {
+      let letterIndex = path.index(path.startIndex, offsetBy: 1)
+      let driveLetter = path[letterIndex]
+      let newPrefix = "/mnt/\(driveLetter)"
+      return newPrefix + path.dropFirst(3)
+    }
+  #endif
+  return path
+}
+
 extension AbsolutePath {
 
   /// Inititializes an absolute path from a string, expanding a leading `~` to `homeDirectoryForCurrentUser` first.
@@ -35,23 +56,6 @@ extension AbsolutePath {
   
   /// Initializes an absolute path from a string, expanding a leading Windows drive letter ('/d:/...') to the WSL equivalent ('/mnt/d/...')
   public init(validatingAndExpandingWSL path: String) throws {
-    #if os(Linux)
-      // Support the special case where a user runs an
-      // editor on Windows with the language server through
-      // Windows Subsystem for Linux (WSL).
-      // In this case, the editor will attempt to pass paths such as
-      // '/d:/...' to the language server which, however, expects
-      // them to be formatted as '/mnt/d/...'.
-      if let _ = windowsPathRegex.firstMatch(in: path, range: NSMakeRange(0, path.length)) {
-        let letterIndex = path.index(path.startIndex, offsetBy: 1)
-        let driveLetter = path[letterIndex]
-        let newPrefix = "/mnt/\(driveLetter)"
-        try self.init(validating: newPrefix + path.dropFirst(3))
-      } else {
-        try self.init(validating: path)
-      }
-    #else
-      try self.init(validating: path)
-    #endif
+    self.init(validating: expandWSL(path: path))
   }
 }
